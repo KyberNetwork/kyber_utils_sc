226a041ef4bb15c2b27dda9cc160e9a8369cd8de
Anton Buenavista <ay.buenavista@gmail.com>
2020-09-02 14:25:24 +0800

    Fixes for broken Travis build

---
+++ .travis/test-part.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# -*- firestarter: "shfmt -i 4 -ci -w %p" -*-
+
+set -euxo pipefail
+
+readonly test_part=${TEST_PART:-}
+
+case "$test_part" in
+Sol6)
+    npx buidler test --no-compile --config buidlerConfigSol6.js
+    ;;
+Coverage)
+    if [[ $TRAVIS_EVENT_TYPE != "push" ]]; then
+        echo "Only running coverage on merge request or direct push"
+    elif [[ $TRAVIS_BRANCH == $COVERAGE_BRANCH ]]; then
+        ./coverage.sh || true
+    else
+        echo "Not running coverage on $TRAVIS_BRANCH"
+    fi
+    ;;
+*)
+    echo "test case not define yet"
+    ;;
+esac
