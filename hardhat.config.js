0948309d765b4c3e014691dd0cc5a76bf8225894
Daniel Pham <daniel.pham765@gmail.com>
2022-03-19 19:23:46 +0700

    Update github action node version

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,34 @@ require('@nomiclabs/hardhat-truffle5');
 require('@nomiclabs/hardhat-web3');
 require('solidity-coverage');
 
-const {testAccounts} = require('./test/wallets');
-
 module.exports = {
   defaultNetwork: 'hardhat',
 
   networks: {
     hardhat: {
-      accounts: testAccounts,
+      accounts: [
+        // 5 accounts with 10^14 ETH each
+        {
+          privateKey: `${process.env.ACCOUNT1}`,
+          balance: '100000000000000000000000000000000',
+        },
+        {
+          privateKey: `${process.env.ACCOUNT2}`,
+          balance: '100000000000000000000000000000000',
+        },
+        {
+          privateKey: `${process.env.ACCOUNT3}`,
+          balance: '100000000000000000000000000000000',
+        },
+        {
+          privateKey: `${process.env.ACCOUNT4}`,
+          balance: '100000000000000000000000000000000',
+        },
+        {
+          privateKey: `${process.env.ACCOUNT5}`,
+          balance: '100000000000000000000000000000000',
+        },
+      ],
     },
   },
 
